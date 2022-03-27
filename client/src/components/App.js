48bdb3da33b741132bc02b090abbf77764d0f863
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2022-03-28 01:59:33 +0200

    blockchain frontend added
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

---
+++ client/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Blocks from './Blocks';
+import logo from '../assets/logo.jpg';
+
+class App extends Component {
+    state = { walletInfo: {} };
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/wallet-info')
+            .then(response => response.json())
+            .then(json => this.setState({ walletInfo: json }));
+    }
+
+    render() {
+        const { address, balance } = this.state.walletInfo;
+
+        return (
+            <div className='App'>
+                <img className='logo' src={logo}></img>
+                <br/>
+                Welcome to the blockchain...
+                <div className='WalletInfo'>
+                    <div>Address: {address}</div>
+                    <div>Balance: {balance}</div>
+                </div>
+                <br/>
+                <Blocks/>
+            </div>
+        );
+    }
+}
+
+export default App;
